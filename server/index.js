246e2b31241c2d5f180cb09fd91f36b4aaf3eb8e
Allison Chen <allisonc07@gmail.com>
2018-08-27 02:52:19 -0700

    Fix endpoints and operation types

--- server/index.js
+++ server/index.js
@@ -11,4 +11,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+app.get('/restaurants', (req, res) => {
+  db.getAllRestuarants((err, results) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+app.post('/newRestaurant', (req, res) => {
+  console.log('post received');
+  db.addRestaurant(req.body.details, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send('new restaurant added!');
+    }
+  });
+});
+
+app.post('/restaurants', (req, res) => {
+  db.updateRestaurant([req.body.name, req.body.date], (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+    }
+  });
+});
+
 module.exports = app;
